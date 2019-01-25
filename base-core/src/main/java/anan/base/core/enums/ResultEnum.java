dec02bf563c0bb281b0b7692d760928ceecec160
anan <anan@anan.com>
2019-01-25 12:30:24 +0800

    1.add Global exception in core; 2.catch DataIntegrityViolationException for Unique index

--- base-core/src/main/java/anan/base/core/enums/ResultEnum.java
+++ base-core/src/main/java/anan/base/core/enums/ResultEnum.java
@@ -9,18 +9,25 @@ import lombok.Getter;
 @Getter
 public enum ResultEnum {
 
+  /**
+   * 不可预测条件
+   */
   SUCCESS(0, "成功"),
   FAILURE(-1,"失败"),
 
 
+  /**
+   * 数据错误导致
+   */
   PARAM_ERROR(1, "参数不正确"),
-
-
   DELETE_SECTION(15, "部分删除失败"),
+  DUPLICATE_ENTRY(16,"已存在，请重试"),
 
 
 
-
+  /**
+   * 登陆情况
+   */
   LOGIN_FAIL(95, "登录失败, 登录信息不正确"),
   LOGOUT_SUCCESS(90, "登出成功"),
 
---
+++ base-core/src/main/java/anan/base/core/exception/ExceptionCatch.java
@@ -0,0 +1,48 @@
+package anan.base.core.exception;
+
+import anan.base.core.enums.ResultEnum;
+import anan.base.core.util.ResultVOUtil;
+import anan.base.core.vo.ResultVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Global exception Catch
+ * you can google them
+ * - ControllerAdvice
+ * - ExceptionHandler
+ *
+ * @author anan
+ * @created by anan on 2019/1/25 10:32
+ */
+
+@ControllerAdvice
+@Slf4j
+public class ExceptionCatch {
+
+  /**
+   * get the DataIntegrityViolationException and catch it
+   * eg:
+   * e.getCause().getCause().getMessage() ==> Duplicate entry 'anan1529' for key 'account'
+   *
+   * @param e
+   * @return
+   */
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  @ResponseBody
+  public ResultVO dataIntegrityViolationException(DataIntegrityViolationException e) {
+    log.error("DataIntegrityViolationException.getMessage=>"+e.getMessage());
+    log.error("DataIntegrityViolationException.getCause.getCause.getMessage=>"+e.getCause().getCause().getMessage());
+
+    if(e.getCause().getCause().getMessage().contains("Duplicate entry") && e.getCause().getCause().getMessage().contains("for key")){
+      String[] ids = e.getCause().getCause().getMessage().split("'");
+      log.error("字段 {} 为唯一索引，存在数据为 {} ，SQL无法执行，已回退", ids[3],ids[1]);
+      return ResultVOUtil.error(ResultEnum.DUPLICATE_ENTRY.getCode(),ids[1]+ResultEnum.DUPLICATE_ENTRY.getMessage());
+    }else {
+      throw new CoreException(ResultEnum.FAILURE.getCode(), ResultEnum.FAILURE.getMessage());
+    }
+  }
+}
